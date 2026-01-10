05e950eb0991759a0670671e84e62ee2bc9ee3e3
GenericCPU <brandongcryderman@gmail.com>
2026-01-10 11:28:18 -0400

    refactor: Use Web Crypto API for secure key generation in auth-utils

--- lib/auth-utils.ts
+++ lib/auth-utils.ts
@@ -1,10 +1,13 @@
 import * as secp256k1 from "@noble/secp256k1"
 
 export function generateAuthenticationKeyPair() {
-  const privateKey = secp256k1.utils.randomPrivateKey()
-  const publicKey = secp256k1.getPublicKey(privateKey, true)
+  // Generate 32 random bytes for the private key
+  const privateKeyBytes = new Uint8Array(32)
+  crypto.getRandomValues(privateKeyBytes)
+
+  const publicKey = secp256k1.getPublicKey(privateKeyBytes, true)
   return {
-    privateKey: Buffer.from(privateKey).toString("hex"),
+    privateKey: Buffer.from(privateKeyBytes).toString("hex"),
     publicKey: Buffer.from(publicKey).toString("hex"),
   }
 }
